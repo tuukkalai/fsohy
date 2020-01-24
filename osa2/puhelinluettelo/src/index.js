7a168d1397b24ed98a121c22a3673195d45dfbb3
Tuukka Laitinen <tuukka.mj.laitinen@gmail.com>
2020-01-24 07:55:17 +0200

    Add puhelinluettelo

---
+++ osa2/puhelinluettelo/src/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const personObj = {
+      name: newName
+    }
+    setPersons(persons.concat(personObj))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(p => <li key={p.name}>{p.name}</li>)}
+      </ul>
+    </div>
+  )
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
