3f54738e5687a0019e3949c64601153c598dba1d
Tuukka Laitinen <tuukka.mj.laitinen@gmail.com>
2020-02-08 22:45:56 +0200

    Update Osa3 3.4-3.8

--- osa3/index.js
+++ osa3/index.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+/*const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}*/
+
+app.use(express.json())
+//app.use(requestLogger)
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
@@ -29,10 +44,39 @@ let persons = [
     }
 ]
 
+
+// First page
 app.get('/', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date}</p>`)
 })
 
+// Persons: Create
+app.post('/api/persons', (req, res) => {
+  if(!req.body.name){
+    return res.status(400).json({
+      error: 'Name missing'
+    })
+  }
+  if(persons.find(p => p.name === req.body.name)){
+    return res.status(400).json({
+      error: 'Name must be unique'
+    })
+  }
+  if(!req.body.number){
+    return res.status(400).json({
+      error: 'Number missing'
+    })
+  }
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+    id: Math.floor(Math.random() * Math.floor(32768))
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+// Persons: Read
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
@@ -40,7 +84,14 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(p => p.id === id)
-  person !== undefined ? res.json(person) : res.send('Person was not found')
+  person !== undefined ? res.json(person) : res.status(404).end()
+})
+
+// Persons: Delete
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
 })
 
 const PORT = 3001
