72652e6b1b8401bcc591c0c5b72527c8fa4059c4
Tuukka Laitinen <tuukka.mj.laitinen@gmail.com>
2020-02-14 13:49:11 +0200

    Add app.js, 4.2

---
+++ osa4/controllers/blogs.js
@@ -0,0 +1,27 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  console.log('request.body', request.body)
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => console.log('Error adding blog to DB', error))
+})
+
+
+module.exports = blogsRouter
--- osa4/index.js
+++ osa4/index.js
@@ -1,39 +1,10 @@
-const config = require('./utils/config')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(console.log('Connected to mongo'))
-  .catch(error => console.log('Error connecting to mongo' , error))
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  console.log('request.body', request.body)
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => console.log('Error adding blog to DB', error))
-})
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
