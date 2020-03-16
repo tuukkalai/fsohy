ab9c48b0a0b53f84b454aa7531e78ba1647992fd
Tuukka Laitinen <tuukka.mj.laitinen@gmail.com>
2020-03-16 11:48:32 +0200

    8.4

--- osa8/index.js
+++ osa8/index.js
@@ -99,7 +99,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author:String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -108,7 +108,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => !args ? books : books.filter(b => b.author === args.author),
     allAuthors: () => authors
   },
   Author: {
