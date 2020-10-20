4f2374717551ef8c2e3723ba445fda5be35ddfb2
Tuukka Laitinen <tuukka.mj.laitinen@gmail.com>
2020-10-20 13:23:00 +0300

    Previous commits that were not pushed

--- osa7/bloglist/front/src/components/Users.js
+++ osa7/bloglist/front/src/components/Users.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Users = ({ user }) => {
   return(
-    <tr><td>{user.username}</td><td>{user.blogs.length}</td></tr>
+    <tr>
+      <td>
+        {user.username}
+      </td>
+      <td>
+        {user.blogs.length}
+      </td>
+    </tr>
   )
 }
 
