0cbd3783697f0f25b5477dda642be8f5e9ad91aa
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2021-02-09 02:29:32 +0200

    Relocating training project. 9.7 almost, missing correct target hours

--- osa9/exerciseCalculator.ts
+++ osa9/exerciseCalculator.ts
@@ -8,10 +8,17 @@ interface ExerciseResult {
   average: number;
 }
 
-const calculateExercises = (training: Array<number>, target: number): ExerciseResult => {
-  const periodLength: number = training.length;
-  const trainingDays: number = training.filter(n => n > 0).length;
-  const average: number = training.reduce((a,b) => (a+b)) / periodLength;
+// 9.3
+interface ExerciseValues {
+  trainingData: Array<number>;
+  target: number;
+}
+
+const calculateExercises = (training: ExerciseValues): ExerciseResult => {
+  const { target } = training;
+  const periodLength: number = training.trainingData.length-1;
+  const trainingDays: number = training.trainingData.filter(n => n > 0).length-1;
+  const average: number = training.trainingData.reduce((a,b) => (a+b)) / periodLength;
   const success: boolean = target <= average;
   const rating: number = 3 - ( target - Math.floor(average) );
   const ratingDescription: string = rating >= 3 ? 'Awesome!' : rating >= 2 ? 'Not too bad!' : 'Maybe next week is better';
@@ -29,12 +36,21 @@ const calculateExercises = (training: Array<number>, target: number): ExerciseRe
 // 9.2
 // console.log(calculateExercises([5, 0, 2, 1.5, 2, 2, 1], 3));
 
-// 9.3
-interface ExerciseValues {
-  trainingData: Array<number>;
-  target: number;
-}
+export const parseInternetArguments = (args: Array<number>): ExerciseValues => {
+  const parsedArgs = [];
+  for (let i = 1; i < args.length; i++) {
+    const arr = Number(args[i]);
+    if (isNaN(arr)) throw new Error('Non-numeric argument');
+    parsedArgs.push(arr);
+  }
+  
+  return {
+    trainingData: parsedArgs,
+    target: Number(args[parsedArgs.length-1])
+  };
+};
 
+/*
 const parseArgs = (args: Array<string>): ExerciseValues => {
   if (args.length < 4) throw new Error('Not enough arguments');
   if (args.length > 10) throw new Error('Too many arguments. One week of training data is maximum.');
@@ -60,3 +76,6 @@ try {
 } catch (e) {
   console.log('Error, something bad happened, message: ', e);
 }
+*/
+
+export default calculateExercises;
--- osa9/index.ts
+++ osa9/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import calculateBmi from './bmiCalculator';
+import calculateExercises, { parseInternetArguments } from './exerciseCalculator';
+
+interface ExerciseCalculatorPost {
+  daily_exercises: number;
+  target: number;
+}
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -12,6 +20,19 @@ app.get('/bmi', (req, res) => {
     res.send(calculateBmi(Number(req.query.height), Number(req.query.weight)));
   } else {
     res.status(401).json({error: 'Malformatted parameters.'});
+  }
+});
+
+app.post('/exercises', (req, res) => {
+  const { daily_exercises, target } = req.body as ExerciseCalculatorPost;
+  if (!daily_exercises || !target)
+    return res.status(400).json({ error: 'parameters missing' });
+  try {
+    const info = parseInternetArguments([2,1,3,4,5,6,7]);
+    const response = calculateExercises(info);
+    return res.json(response);
+  } catch {
+    return res.status(400).json({ error: 'malformatted parameters' });
   }
 });
 
